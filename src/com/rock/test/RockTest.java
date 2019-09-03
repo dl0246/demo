35043b63a3844925c2345d678f1446c1cb1cf4e3
dl0246 <dl0246@163.com>
2019-09-03 17:02:58 +0800

    eclipse github版本库管理

---
+++ src/com/rock/test/RockTest.java
@@ -0,0 +1,28 @@
+package com.rock.test;
+
+/**FileName:	com.rock.test 	RockTest.java
+ * TODO:		
+ * Copyright:	Copyright (c) 2015-2016 All Rights Reserved. Company: 01skill-soft.com Inc.
+ * @author: 	老张
+ * @Date:		2019年9月3日:下午5:00:44
+ * @version: 	1.0
+ * 
+ *           Modification History: Date			Author 		Version 	Description
+ *           ----------------------------------------------------------------------
+ *           					   2019年9月3日 	老张	 		1.0 		1.0 Version
+ * 
+ */
+public class RockTest {
+
+	/**
+	 * @TODO	 :
+	 * @Date	 :2019年9月3日 下午5:00:44
+	 * @Author	 :老张
+	 * @param args   :
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("RockTest.main()");
+	}
+
+}
